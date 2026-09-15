d9254d0e32310a21bdf1c2c614995de34c2519bc
GitHub Action <action@github.com>
2026-09-15 12:10:22 +0000

    Build

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -112,4 +112,10 @@ var store = [{
         "tags": [],
         "url": "/2026/06/30/Sigcomm-poster.html",
         "teaser": null
+      },{
+        "title": "MCCL: Measurement-based Collective Communication Library.",
+        "excerpt":"Our Paper titled ” MCCL: Measurement-based Collective Communication Library “ will be presented at Conext 2026 Congrats to the team, especially Michele Ferrero. Abstract: Collective communication is a critical performance bottleneck in large-scale distributed training. Existing libraries and synthesis frameworks rely on static schedules optimized for peak network conditions. In...","categories": [],
+        "tags": [],
+        "url": "/2026/09/10/Conext.html",
+        "teaser": null
       }]
